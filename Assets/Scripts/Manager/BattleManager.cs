4ac2ecb9dd78159d84d7d49ed834b87786ddb448
DESKTOP-P0CK6VC\Sei Adachi <sei.dance.dream@gmail.com>
2022-12-06 20:21:11 +0900

    バトルマネージャー制作中

--- Assets/Scripts/Manager/BattleManager.cs
+++ Assets/Scripts/Manager/BattleManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
 using UnityEngine;
 
 /// <summary>
@@ -7,18 +8,107 @@ using UnityEngine;
 /// </summary>
 public class BattleManager : MonoBehaviour
 {
-    //手札選択フェーズ...キャラクターとじゃんけんのカードを選択
-    //カード選択フェーズ...じゃんけんカードを選ぶ
-    //バトル勝敗決定処理フェーズ...勝敗を決める
+    //手札選択フェーズ(試合開始前)...キャラクターとじゃんけんのカードを選択
+
+    ///ターン開始///  
+    //カード選択フェーズ(メインフェーズ)...じゃんけんカードの選択、リザーブのチェック、リーダー効果確認
+
+    //20秒経過したらじゃんけんカードをセット
+    //待機
+
+    //バトル勝敗決定処理フェーズ(バトルフェーズ)...勝敗を決める
     //勝者のダメージ処理フェーズ...ダメージ処理
-    //勝者のカード効果処理フェーズ...じゃんけんカードの処理
-    //キャラクターのカード効果処理フェーズ...キャラクターのカードの処理
-    //リザーブ処理フェーズ...カードを捨てる
-    //リフレッシュ処理フェーズ...
-    //決着処理フェーズ...
+    //勝者のカード効果処理フェーズ...じゃんけんカードの処理(ダメージでも回復でもなければストック)
+    //キャラクターのカード効果処理フェーズ...キャラクターのカードの処理(ダメージでも回復でもなければストック)
+
+    //HPが0になっていたらゲームエンド
+
+    //リザーブ処理フェーズ...ストックした処理を行う
+    //リフレッシュ処理フェーズ...カードを捨てる
+    //決着処理フェーズ...ターン終了処理
+    ///ターン終了(カード選択フェーズに戻る)///
+
+    //介入処理フェーズ...介入処理がある場合に
+
+    [SerializeField]
+    [Header("手札選択フェーズ強制終了時間")]
+    float _handSelectTime = 30f;
+
+    [SerializeField]
+    [Header("カード選択フェーズ強制終了時間")]
+    float _cardSelectTime = 20f;
+
+    [SerializeField]
+    [Header("バトル勝敗決定処理フェーズ終了時間")]
+    float _battleTime = 5f;
+
+    [SerializeField]
+    [Header("勝者のダメージ処理フェーズ終了時間")]
+    float _winnerDamegeProcessTime = 5f;
+
+    [SerializeField]
+    [Header("勝者のカード効果フェーズ終了時間")]
+    float _winnerCardEffectTime = 5f;
+
+    [SerializeField]
+    [Header("キャラクターの効果フェーズ終了時間")]
+    float _characterEffectTime = 5f;
+
+    [SerializeField]
+    [Header("リザーブ処理フェーズ終了時間")]
+    float _useCardOnReserveTime = 5f;
+
+    [SerializeField]
+    [Header("リフレッシュ処理フェーズ終了時間")]
+    float _refreshTime = 5f;
+
+    [SerializeField]
+    [Header("決着処理フェーズ終了時間")]
+    float _judgementTime = 5f;
+
+    [SerializeField]
+    [Header("介入処理フェーズ終了時間")]
+    float _interventionTime = 20f;
+
+    private void Awake()
+    {
+        HandSelect();
+    }
+
+    /// <summary>
+    /// カードを選択
+    /// </summary>
+
+    async private void HandSelect()
+    {
+        await Delay(_handSelectTime);
+    }
+
+    /// <summary>
+    /// 出すカードを決める
+    /// </summary>
+    private void CardSelect()
+    {
+
+    }
 
     private void Battle()
     {
-        //PlayerManager.Players[0].
+
+    }
+
+    async private UniTask Delay(float delayTime)
+    {
+        for (float i = 0f; i < delayTime; i += Time.deltaTime)
+        {
+            foreach (var player in PlayerManager.Players)
+            {
+                if(player.CharacterHand != null)
+                {
+
+                }
+            }
+            await UniTask.NextFrame();
+        }
     }
 }
