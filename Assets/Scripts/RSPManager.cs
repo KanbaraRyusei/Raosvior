eeb70480b12ed375b8ce8b3dc4aac43159399615
DESKTOP-P0CK6VC\Sei Adachi <sei.dance.dream@gmail.com>
2023-01-17 18:06:07 +0900

    じゃんけんの勝敗を手助けしてくれるマネージャーを作った

---
+++ Assets/Scripts/RSPManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// じゃんけんの勝敗を手助けしてくれるマネージャー
+/// </summary>
+public struct RSPManager
+{
+    #region Constants
+
+    public const int WIN = 2;
+    public const int DRAW = 0;
+    public const int LOSE = 1;
+
+    private const int RSP_OFFSET = 3;
+    private const int RSP_REMAINDER = 3;
+
+    #endregion
+
+    #region Public Method
+
+    public static int Calculator(RSPParameter playerRSP,RSPParameter enemyRSP)
+    {
+        var value = (playerRSP - enemyRSP + RSP_OFFSET) % RSP_REMAINDER;
+        return value;
+    }
+
+    #endregion
+}
